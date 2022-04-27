37f88e31fd378024f610db0c640d4bc9b0f8d1bd
danjab-jd <jd@pja.edu.pl>
2022-04-27 21:25:45 +0200

    init project

---
+++ DbFirst/Controllers/BooksController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DbFirst.DTO;
+using DbFirst.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DbFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookDbService _bookDbService;
+
+        public BooksController(IBookDbService bookDbService)
+        {
+            _bookDbService = bookDbService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooksList()
+        {
+            IList<BookDTO> result = await _bookDbService.GetBooksListAsync();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBook(BookDTO bookDTO)
+        {
+            await _bookDbService.AddBookAsync(bookDTO);
+
+            return Ok();
+        }
+
+        [HttpPut("{idBook}")]
+        public async Task<IActionResult> UpdateBook(BookDTO bookDTO)
+        {
+            await _bookDbService.UpdateBookAsync(bookDTO);
+
+            return Ok();
+        }
+    }
+}
