f5bf46506ca115b4ac047a54decf63e1a15cb829
danjab-jd <jd@pja.edu.pl>
2022-04-28 11:46:28 +0200

    Working with ef db first

---
+++ DbFirst/DTO/AuthorDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DbFirst.DTO
+{
+    public class AuthorDTO
+    {
+        public int IdAuthor { get; set; }
+        public string FirstName { get; set; }
+    }
+}
--- DbFirst/Services/BookDbService.cs
+++ DbFirst/Services/BookDbService.cs
@@ -1,24 +1,85 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DbFirst.DTO;
+using DbFirst.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DbFirst.Services
 {
     public class BookDbService : IBookDbService
     {
-        public Task<IList<BookDTO>> GetBooksListAsync()
+        private readonly jdContext _context;
+
+        public BookDbService(jdContext context)
         {
-            throw new System.NotImplementedException();
+            _context = context;
         }
 
-        public Task AddBookAsync(BookDTO bookDTO)
+        public async Task<IList<BookDTO>> GetBooksListAsync()
         {
-            throw new System.NotImplementedException();
+            /*
+             * await _context.ClientTrips
+             * .Include(x => x.IdClientNavigation)
+             * .Include(x => x.IdTripNavigation)
+             */
+
+            /*
+             * await _context.Books
+             * .Include(x => x.IdAuthorNavigation).ThenInclude(x=>x.IdCityDictNav)
+             * .Include(x => x.IdPublisherNav)
+             */
+
+            return await _context.Books
+                .Include(x => x.IdAuthorNavigation)
+                .Select(x => new BookDTO
+                {
+                    IdBook = x.IdBook,
+                    Title = x.Title,
+                    Author = new()
+                    {
+                        IdAuthor = x.IdAuthor,
+                        FirstName = x.IdAuthorNavigation.Name
+                    }
+                })
+                .ToListAsync();
         }
 
-        public Task UpdateBookAsync(BookDTO bookDTO)
+        public async Task AddBookAsync(BookDTO bookDTO)
         {
-            throw new System.NotImplementedException();
+            Author authorFromDb = await _context.Authors
+                .SingleOrDefaultAsync(x => x.IdAuthor == bookDTO.Author.IdAuthor);
+
+            if(authorFromDb == null)
+            {
+                // nie znaleziono
+                return;
+            }
+
+            await _context.Books.AddAsync(new Book
+            { 
+                Title = bookDTO.Title,
+                IdAuthorNavigation = authorFromDb
+            });
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBookAsync(BookDTO bookDTO)
+        {
+            Book bookFromDb = await _context.Books
+                .SingleOrDefaultAsync(x => x.IdBook == bookDTO.IdBook);
+
+            if(bookFromDb == null)
+            {
+                // nie ma ksiazki
+                return;
+            }
+
+            bookFromDb.Title = bookDTO.Title;
+            // zmieniam properties
+
+            await _context.SaveChangesAsync();
         }
     }
 }
